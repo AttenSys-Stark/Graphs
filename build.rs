32821217de3b96272fe33e7af112138336ad8a2d
Akinbola247 <akinbolakehinde247@gmail.com>
2025-04-23 14:55:56 +0100

    v3 migration

--- build.rs
+++ build.rs
@@ -5,21 +5,21 @@ fn main() {
     // Aliases added from the ABI
     let mut aliases = HashMap::new();
     aliases.insert(
-        String::from("openzeppelin_access::ownable::ownable::OwnableComponent::Event"),
-        String::from("OwnableComponentEvent"),
-    );
-    aliases.insert(
         String::from("openzeppelin_upgrades::upgradeable::UpgradeableComponent::Event"),
         String::from("UpgradeableComponentEvent"),
     );
+    aliases.insert(
+        String::from("openzeppelin_access::ownable::ownable::OwnableComponent::Event"),
+        String::from("OwnableComponentEvent"),
+    );
 
-    let orgsubgraph_abigen =
-        Abigen::new("orgsubgraph", "./abi/orgsubgraph_contract.abi.json").with_types_aliases(aliases).with_derives(vec!["serde::Serialize".to_string(), "serde::Deserialize".to_string()]);
+    let coursesubgraph_abigen =
+        Abigen::new("coursesubgraph", "./abi/coursesubgraph_contract.abi.json").with_types_aliases(aliases).with_derives(vec!["serde::Serialize".to_string(), "serde::Deserialize".to_string()]);
 
-        orgsubgraph_abigen
+        coursesubgraph_abigen
             .generate()
             .expect("Fail to generate bindings")
-            .write_to_file("./src/abi/orgsubgraph_contract.rs")
+            .write_to_file("./src/abi/coursesubgraph_contract.rs")
             .unwrap();
     // Aliases added from the ABI
     let mut aliases = HashMap::new();
@@ -51,12 +51,12 @@ fn main() {
         String::from("UpgradeableComponentEvent"),
     );
 
-    let coursesubgraph_abigen =
-        Abigen::new("coursesubgraph", "./abi/coursesubgraph_contract.abi.json").with_types_aliases(aliases).with_derives(vec!["serde::Serialize".to_string(), "serde::Deserialize".to_string()]);
+    let orgsubgraph_abigen =
+        Abigen::new("orgsubgraph", "./abi/orgsubgraph_contract.abi.json").with_types_aliases(aliases).with_derives(vec!["serde::Serialize".to_string(), "serde::Deserialize".to_string()]);
 
-        coursesubgraph_abigen
+        orgsubgraph_abigen
             .generate()
             .expect("Fail to generate bindings")
-            .write_to_file("./src/abi/coursesubgraph_contract.rs")
+            .write_to_file("./src/abi/orgsubgraph_contract.rs")
             .unwrap();
 }
